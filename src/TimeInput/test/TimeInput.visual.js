31df3609d30b273d2f23031ee6282b03dc2ecb83
Daniel Eliraz <danielel@b7-c02zc0wqlvdr.lan.wixpress.com>
2020-02-11 11:28:30 +0200

    Custom suffix in TimePicker

--- src/TimeInput/test/TimeInput.visual.js
+++ src/TimeInput/test/TimeInput.visual.js
@@ -7,6 +7,8 @@ import { snap, visualize } from 'storybook-snapper';
 const value1 = moment(0).set({ hour: 9, minute: 45 });
 const value2 = moment(0).set({ hour: 16, minute: 21 });
 
+const customSuffix = <div>custom suffix</div>;
+
 visualize('TimeInput', () => {
   snap('different variations', done => {
     document.fonts.onloadingdone = done;
@@ -20,6 +22,14 @@ visualize('TimeInput', () => {
         <br />
         <TimeInput defaultValue={value1} disableAmPm />
         <br />
+        <TimeInput
+          defaultValue={value1}
+          disableAmPm
+          customSuffix={customSuffix}
+        />
+        <br />
+        <TimeInput defaultValue={value1} customSuffix={customSuffix} />
+        <br />
         <TimeInput defaultValue={value2} rtl />
         <br />
         <TimeInput defaultValue={value2} rtl disabled />
@@ -27,6 +37,15 @@ visualize('TimeInput', () => {
         <TimeInput defaultValue={value2} rtl disabled dashesWhenDisabled />
         <br />
         <TimeInput defaultValue={value2} rtl disableAmPm />
+        <br />
+        <TimeInput defaultValue={value1} customSuffix={customSuffix} />
+        <br />
+        <TimeInput
+          defaultValue={value1}
+          rtl
+          disableAmPm
+          customSuffix={customSuffix}
+        />
       </>
     );
   });
