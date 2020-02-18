d053849da648944ce44eb8a1c1ae92f11f3a5fd9
Egozi <egozieran@gmail.com>
2020-02-18 15:17:47 +0200

    Remove composite components (#5019)

--- testkit/component-definitions.js
+++ testkit/component-definitions.js
@@ -40,18 +40,6 @@ export default {
     },
   },
 
-  MultiSelectComposite: {
-    props: {
-      children: <MultiSelect />,
-    },
-  },
-
-  AutoCompleteComposite: {
-    props: {
-      children: [<AutoComplete key={1} />],
-    },
-  },
-
   Popover: {
     props: {
       children: [
@@ -321,12 +309,6 @@ export default {
   Box: {
     props: {
       children: <div />,
-    },
-  },
-
-  GoogleAddressInputWithLabel: {
-    props: {
-      children: <GoogleAddressInput Client={GmapsTestClient} />,
     },
   },
 
