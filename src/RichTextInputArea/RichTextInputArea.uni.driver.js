2292eb434333d889ffe54789c9c17533d35b05c4
Egozi <egozi@wix.com>
2020-02-27 17:22:44 +0200

    Small changes as a preparation to wsr8

--- src/RichTextInputArea/RichTextInputArea.uni.driver.js
+++ src/RichTextInputArea/RichTextInputArea.uni.driver.js
@@ -1,26 +1,35 @@
 import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
 import ReactTestUtils from 'react-dom/test-utils';
 
-import { errorIndicatorDriverFactory } from '../ErrorIndicator/ErrorIndicator.uni.driver';
+import { statusIndicatorDriverFactory } from '../StatusIndicator/StatusIndicator.uni.driver';
+import { statusTypes } from '../VariableInput/constants';
 
 export const getContent = base => base.$('.public-DraftEditor-content');
 export const getPlaceholder = base =>
   base.$('.public-DraftEditorPlaceholder-root');
 
-const getErrorIndicator = base =>
-  base.$('[data-hook=richtextarea-error-indicator]');
-const errorIndicatorDriver = (base, body) =>
-  errorIndicatorDriverFactory(getErrorIndicator(base), body);
-
 export default (base, body) => {
+  const statusIndicatorDriver = statusIndicatorDriverFactory(
+    base.$(`[data-hook="richtextarea-error-indicator"]`),
+    body,
+  );
+
   return {
     ...baseUniDriverFactory(base, body),
     isDisabled: async () =>
       Boolean(await getContent(base).attr('contenteditable')),
-    hasError: async () => await getErrorIndicator(base).exists(),
+    hasError: async () => {
+      const exists = await statusIndicatorDriver.exists();
+      if (exists) {
+        const status = await statusIndicatorDriver.getStatus();
+        return status === statusTypes.error;
+      }
+
+      return false;
+    },
     getContent: () => getContent(base).text(),
     getPlaceholder: () => getPlaceholder(base).text(),
-    getErrorMessage: () => errorIndicatorDriver(base, body).getErrorMessage(),
+    getErrorMessage: statusIndicatorDriver.getMessage,
     enterText: async text => {
       const contentElement = await getContent(base).getNative(); // eslint-disable-line no-restricted-properties
 
