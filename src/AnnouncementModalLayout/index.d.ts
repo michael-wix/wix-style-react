f8714c140a466fe157c1e8ae50fff88140c29127
itaytay <itayf@wix.com>
2020-03-09 09:45:10 +0200

    <AnnouncementModalLayout /> - type fixes

--- src/AnnouncementModalLayout/index.d.ts
+++ src/AnnouncementModalLayout/index.d.ts
@@ -5,7 +5,7 @@ export interface AnnouncementModalLayoutProps extends BaseModalLayoutProps{
   className?: string,
   dataHook?: string,
   illustration?: React.ReactNode,
-  theme?: 'standard' | 'destructive' | 'premium',
+  theme?: 'standard' | 'premium',
 }
 
 export default class AnnouncementModalLayout extends React.PureComponent<AnnouncementModalLayoutProps>{}
